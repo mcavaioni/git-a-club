8c0c066978c6e80e8dfd52fef8f0c3bca0564115
chenghw <ho.won.cheng@gmail.com>
2015-12-02 18:24:00 -0500

    completed listing show, listing reservation new/create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,24 @@
 class ReservationsController < ApplicationController
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @reservation = Reservation.new
+  end
+
+  def create
+    @listing = Listing.find(params[:listing_id])
+    @reservation = @listing.reservations.build(reservation_params)
+    @reservation.save
+    redirect_to listing_reservation_path(@listing, @reservation)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :finish_date)
+  end
+
 end
