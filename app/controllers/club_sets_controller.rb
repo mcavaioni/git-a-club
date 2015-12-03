a5421a9cc3fe6c3235fd65839c399915b2619fb1
chenghw <ho.won.cheng@gmail.com>
2015-12-03 18:13:34 -0500

    club set new and create started

--- app/controllers/club_sets_controller.rb
+++ app/controllers/club_sets_controller.rb
@@ -1,2 +1,23 @@
-class ClubSetController < ApplicationController
+class ClubSetsController < ApplicationController
+  before_action :find_supplier, only: [:new, :create]
+
+  def new
+    @club_set = ClubSet.new
+  end
+
+  def create
+    binding.pry
+  end
+
+  def show
+
+  end
+
+  private
+
+  def find_supplier
+    @supplier = Supplier.find(params[:supplier_id])
+  end
+
+  
 end
