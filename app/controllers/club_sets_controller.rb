fdd10647e25e9b670d4e136ca7bee0e0c20f17c6
chenghw <ho.won.cheng@gmail.com>
2015-12-04 14:55:26 -0500

    club sets addition/delete complete

--- app/controllers/club_sets_controller.rb
+++ app/controllers/club_sets_controller.rb
@@ -1,5 +1,6 @@
 class ClubSetsController < ApplicationController
-  before_action :find_supplier, only: [:new, :create, :show]
+  before_action :find_supplier, only: [:new, :create, :show, :destroy]
+  before_action :find_club_set, only: [:show, :destroy]
 
   def new
     @club_set = ClubSet.new
@@ -12,17 +13,24 @@ class ClubSetsController < ApplicationController
   end
 
   def show
-    @club_set = ClubSet.find(params[:id])
+
   end
 
   def destroy
     @club_set.active = false
+    @club_set.save
+    flash.now[:notice] = "Your club set has been removed."
+    redirect_to @supplier
   end
 
   private
 
   def club_set_params
     params.require(:club_set).permit(:name, :summary, :picture, :club_ids => [])
+  end
+
+  def find_club_set
+    @club_set = ClubSet.find(params[:id])
   end
 
   def find_supplier
