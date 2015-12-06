b1d3c63038558636fc70101c899390b4ea443ac2
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-12-06 14:59:57 -0500

    Add lazy loader to listings page

--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -1,11 +1,24 @@
 $(function(){
+
+  $.get('/listings/four_listings/0', function(data){
+    $('.search-results').append(data.new_row)
+  })
+
+  $('.load-listings').on('click', function(){
+    row_number = $('.search-results .row').length;
+    debugger;
+    $.get('/listings/four_listings/'+row_number*4, function(data){
+      $('.search-results').append(data.new_row);
+    })
+  })
+
   $('.button-class').on ('click', function(){
     var $form = $(this).first().siblings()
     $form.toggleClass("hide-form")
     $(this).toggleClass("hide-button")
 
   })
-  
+
   $('.btn-cancel').on ('click', function(){
 
     var $form = $(this).parent()
