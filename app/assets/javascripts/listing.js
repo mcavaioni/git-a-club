fa52cb038e37e41050717abce59df28fef55ab64
mcavaioni <michele.cavaioni@gmail.com>
2015-12-06 11:46:48 -0500

    initial ajax code

--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -1,16 +1,28 @@
-$(function(){
-  $('.button-class').on ('click', function(){
-    var $form = $(this).first().siblings()
-    $form.toggleClass("hide-form")
-    $(this).toggleClass("hide-button")
+// $(function(){
+//   $('.button-class').on ('click', function(){
+//     var $form = $(this).first().siblings()
+//     $form.toggleClass("hide-form")
+//     $(this).toggleClass("hide-button")
 
-  })
+//   })
   
-  $('.btn-cancel').on ('click', function(){
+//   $('.btn-cancel').on ('click', function(){
 
-    var $form = $(this).parent()
-    $form.toggleClass("hide-form")
-    var $button = $form.siblings()
-    $button.toggleClass("hide-button")
+//     var $form = $(this).parent()
+//     $form.toggleClass("hide-form")
+//     var $button = $form.siblings()
+//     $button.toggleClass("hide-button")
+//   })
+// })
+
+$(function (){
+  $("#new_listing").on ('ajax:success', function(event, data, status, xhr){
+    debugger;
+
+
   })
+
+
 })
+
+// $('.add-btn-js')
