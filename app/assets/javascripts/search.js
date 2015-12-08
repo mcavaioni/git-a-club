65d76481a1cd6c323304abac22b604ba3b8c3b49
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-12-08 13:43:39 -0500

    Add test for #sanitize_search_terms

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,21 +4,13 @@ $(function(){
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
     var listings = data;
-    $('.search-results').html('')
-    var listing_results = ""
+    $('.search-results').html('');
+    var listing_results = "";
     listings.forEach(function(listing){
       var listing_html = template(listing);
-      listing_results = listing_results + listing_html
+      listing_results = listing_results + listing_html;
     })
     $(listing_results).wrap("<ul></ul>").appendTo('.search-results')
-    // load results
-    // apply checkbox criteria
+    attachCalendar();
   })
 })
-
-
-  // $('#new_product').on('ajax:success', function(event, data, status, xhr){
-  //   $(this).find('input:text').val('');
-  //   var template = $(data.template)
-  //   $('.container').append(template);
-  // })
