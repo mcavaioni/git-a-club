d32c9c139526faef770112141c1bcfe44cd363a6
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-12-08 09:06:56 -0500

    Handlebars working

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,16 @@
 $(function(){
 
   $("form#listings-search").on('ajax:success', function(event, data, status, xhr){
-    debugger;
+    var source = $('#entry-template').html();
+    var template = Handlebars.compile(source);
+    var listings = data;
+    $('.search-results').html('')
+    var listing_results = ""
+    listings.forEach(function(listing){
+      var listing_html = template(listing);
+      listing_results = listing_results + listing_html
+    })
+    $(listing_results).wrap("<ul></ul>").appendTo('.search-results')
     // load results
     // apply checkbox criteria
   })
