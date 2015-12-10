683f35537c2a84866be2f902ed2760c45f267ba3
mcavaioni <michele.cavaioni@gmail.com>
2015-12-10 09:58:36 -0500

    added css file

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,14 +1,20 @@
 
 $(function(){
 
+  $('.btn-to-update').on ('click', function(){
+debugger;
+      $(this).addClass('displaying')
+  })
+  
 $("form.edit_user").on ('ajax:success', function(event, data, status, xhr){
   
+
   var template = $(data.template)
-  debugger;
+  // debugger;
   $(this).parents('.parent-js').find('.thisone').html($(template))
 
-    debugger;
-  $('.thisone').on('dbclick', function (){
+    // debugger;
+  $('.user_form').on('dbclick', function(){
     $(this).parents('.thisone').addClass('displaying')
 
   })
