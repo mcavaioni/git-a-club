73300368b712d157157e4cb1b9fb2d340edae76b
chenghw <ho.won.cheng@gmail.com>
2015-12-15 10:01:06 -0500

    fixed clubs urls

--- app/view_objects/clubs_json_view_object.rb
+++ app/view_objects/clubs_json_view_object.rb
@@ -19,6 +19,7 @@ class ClubsJsonViewObject
       shaft_stiffness: club.generic_club.shaft_stiffness.capitalize,
       condition: club.condition.capitalize,
       url: "/suppliers/#{club.supplier_id}/clubs/#{club.id}/listings/new",
+      club_url: "/suppliers/#{club.supplier_id}/clubs/#{club.id}",
       obj_class: club.class.to_s
     }
   end
