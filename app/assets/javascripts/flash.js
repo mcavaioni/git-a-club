649d0a99c78384bcb5355bf07c0f1928e2e68e71
chenghw <ho.won.cheng@gmail.com>
2015-12-30 15:03:17 -0500

    removed all coffeescript files

--- app/assets/javascripts/flash.js
+++
@@ -1,8 +0,0 @@
-// Currently not being used.
-
-// $(function(){
-//   $('form').on('ajax:success', function(event, data, status, xhr) {
-//     refreshCalendar(data.dateData);
-//     $('#flash-notice').text(data.notice);
-//   });
-// })
