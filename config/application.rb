46c454628f7510a27037d73b0b70f65352bc201e
chenghw <ho.won.cheng@gmail.com>
2015-12-30 15:00:07 -0500

    add config.serve_static_assets = true

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module GitAClub
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.serve_static_assets = true
   end
 end
